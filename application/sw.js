c1f825a5fe3d70d773fe62209c02693478ad8453
XXanderWP <XXanderWP@users.noreply.github.com>
2026-08-10 23:26:40 +0000

    deploy: RocketMan-System/VPN@58d1b0d21a70444f5045e416c16665cdd975c54d

--- application/sw.js
+++ application/sw.js
@@ -1,5 +1,5 @@
 
-console.log("%cBuild date: 8/10/2026, 11:12:45 PM", "color: #4CAF50; font-weight: bold;");
+console.log("%cBuild date: 8/10/2026, 11:26:22 PM", "color: #4CAF50; font-weight: bold;");
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 
