5145e0683578adb2777477031b2480f3dae587b0
XXanderWP <XXanderWP@users.noreply.github.com>
2026-08-12 23:31:24 +0000

    deploy: RocketMan-System/VPN@971e8b9e77a6989d7a9afbe245beff161e130533

--- application/telegram-web-app.js
+++ application/telegram-web-app.js
@@ -1,5 +1,5 @@
 
-console.log("%cBuild date: 8/12/2026, 12:52:09 AM", "color: #4CAF50; font-weight: bold;");
+console.log("%cBuild date: 8/12/2026, 11:31:22 PM", "color: #4CAF50; font-weight: bold;");
 // WebView
 (function () {
   var eventHandlers = {};
